0c33b1cfe0ab746424df97acede170f4f4c19ca0
Hoel Kervadec <hoel@kervadec.science>
2019-12-23 16:53:59 -0500

    Add cityscape colormap

--- viewer.py
+++ viewer.py
@@ -5,6 +5,7 @@ import argparse
 from pathlib import Path
 from pprint import pprint
 from functools import partial
+from collections import namedtuple
 from typing import Callable, Dict, List, Tuple
 
 import numpy as np
@@ -15,6 +16,51 @@ from skimage.io import imread
 from skimage.transform import resize
 from matplotlib.widgets import Button
 from mpl_toolkits.axes_grid1 import ImageGrid
+from matplotlib.colors import ListedColormap
+
+
+# Based on torchvision, itself based on
+# Based on https://github.com/mcordts/cityscapesScripts
+CityscapesClass = namedtuple('CityscapesClass', ['name', 'id', 'train_id', 'category', 'category_id',
+                             'has_instances', 'ignore_in_eval', 'color'])
+
+classes = [
+    CityscapesClass('unlabeled', 0, 255, 'void', 0, False, True, (0, 0, 0)),
+    CityscapesClass('ego vehicle', 1, 255, 'void', 0, False, True, (0, 0, 0)),
+    CityscapesClass('rectification border', 2, 255, 'void', 0, False, True, (0, 0, 0)),
+    CityscapesClass('out of roi', 3, 255, 'void', 0, False, True, (0, 0, 0)),
+    CityscapesClass('static', 4, 255, 'void', 0, False, True, (0, 0, 0)),
+    CityscapesClass('dynamic', 5, 255, 'void', 0, False, True, (111, 74, 0)),
+    CityscapesClass('ground', 6, 255, 'void', 0, False, True, (81, 0, 81)),
+    CityscapesClass('road', 7, 0, 'flat', 1, False, False, (128, 64, 128)),
+    CityscapesClass('sidewalk', 8, 1, 'flat', 1, False, False, (244, 35, 232)),
+    CityscapesClass('parking', 9, 255, 'flat', 1, False, True, (250, 170, 160)),
+    CityscapesClass('rail track', 10, 255, 'flat', 1, False, True, (230, 150, 140)),
+    CityscapesClass('building', 11, 2, 'construction', 2, False, False, (70, 70, 70)),
+    CityscapesClass('wall', 12, 3, 'construction', 2, False, False, (102, 102, 156)),
+    CityscapesClass('fence', 13, 4, 'construction', 2, False, False, (190, 153, 153)),
+    CityscapesClass('guard rail', 14, 255, 'construction', 2, False, True, (180, 165, 180)),
+    CityscapesClass('bridge', 15, 255, 'construction', 2, False, True, (150, 100, 100)),
+    CityscapesClass('tunnel', 16, 255, 'construction', 2, False, True, (150, 120, 90)),
+    CityscapesClass('pole', 17, 5, 'object', 3, False, False, (153, 153, 153)),
+    CityscapesClass('polegroup', 18, 255, 'object', 3, False, True, (153, 153, 153)),
+    CityscapesClass('traffic light', 19, 6, 'object', 3, False, False, (250, 170, 30)),
+    CityscapesClass('traffic sign', 20, 7, 'object', 3, False, False, (220, 220, 0)),
+    CityscapesClass('vegetation', 21, 8, 'nature', 4, False, False, (107, 142, 35)),
+    CityscapesClass('terrain', 22, 9, 'nature', 4, False, False, (152, 251, 152)),
+    CityscapesClass('sky', 23, 10, 'sky', 5, False, False, (70, 130, 180)),
+    CityscapesClass('person', 24, 11, 'human', 6, True, False, (220, 20, 60)),
+    CityscapesClass('rider', 25, 12, 'human', 6, True, False, (255, 0, 0)),
+    CityscapesClass('car', 26, 13, 'vehicle', 7, True, False, (0, 0, 142)),
+    CityscapesClass('truck', 27, 14, 'vehicle', 7, True, False, (0, 0, 70)),
+    CityscapesClass('bus', 28, 15, 'vehicle', 7, True, False, (0, 60, 100)),
+    CityscapesClass('caravan', 29, 255, 'vehicle', 7, True, True, (0, 0, 90)),
+    CityscapesClass('trailer', 30, 255, 'vehicle', 7, True, True, (0, 0, 110)),
+    CityscapesClass('train', 31, 16, 'vehicle', 7, True, False, (0, 80, 100)),
+    CityscapesClass('motorcycle', 32, 17, 'vehicle', 7, True, False, (0, 0, 230)),
+    CityscapesClass('bicycle', 33, 18, 'vehicle', 7, True, False, (119, 11, 32)),
+    CityscapesClass('license plate', -1, -1, 'vehicle', 7, False, True, (0, 0, 142)),
+]
 
 
 def extract(pattern: str, string: str) -> str:
@@ -24,7 +70,8 @@ def extract(pattern: str, string: str) -> str:
         return None
 
 
-def display_item(axe, img: np.ndarray, mask: np.ndarray, contour: bool, args):
+def display_item(axe, img: np.ndarray, mask: np.ndarray, contour: bool, cmap,
+                 args):
     m = resize(mask, img.shape[:2], mode='constant', preserve_range=True)
     # try:
     #     assert len(img.shape) == len(m.shape)
@@ -41,9 +88,9 @@ def display_item(axe, img: np.ndarray, mask: np.ndarray, contour: bool, args):
     axe.imshow(img, cmap="gray")
 
     if contour:
-        axe.contour(m, cmap=args.cmap)
+        axe.contour(m, cmap=cmap)
     else:
-        axe.imshow(m, cmap=args.cmap, alpha=args.alpha, vmin=0, vmax=args.C)
+        axe.imshow(m, cmap=cmap, alpha=args.alpha, vmin=0, vmax=args.C)
     axe.axis('off')
 
 
@@ -77,7 +124,15 @@ def display(background_names: List[str], segmentation_names: List[List[str]],
                 for k, v in remap.items():
                     seg[seg == k] = v
 
-            display_item(axe, img, seg, contour, args)
+            if args.cmap == 'cityscape':
+                colors = [tuple(c / 255 for c in e.color) for e in classes]
+                names = [e.name for e in classes]
+
+                cmap = ListedColormap(colors, names)
+            else:
+                cmap = args.cmap
+
+            display_item(axe, img, seg, contour, cmap, args)
 
             if j == 0:
                 print(row_title[idx])
@@ -162,7 +217,7 @@ def get_args() -> argparse.Namespace:
                         help="Do not draw a contour but a transparent overlap instead.")
     parser.add_argument("--alpha", default=0.5, type=float)
     parser.add_argument("-C", default=2, help="Number of classes. Useful when not all of them appear on each images.")
-    parser.add_argument("--cmap", default='rainbow', choices=list(cm.datad.keys()))
+    parser.add_argument("--cmap", default='rainbow', choices=list(cm.datad.keys()) + ['cityscape'])
     parser.add_argument("--remap", type=str, default="{}",
                         help="Remap some mask values if needed. Useful to suppress some classes.")
     args = parser.parse_args()
