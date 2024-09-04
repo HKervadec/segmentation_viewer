d652f5ba1caed3ec7b0d75a1359e627e4e9474f8
Hoel Kervadec <hoel@kervadec.science>
2024-09-04 13:33:30 +0200

    Update license year

--- viewer.py
+++ viewer.py
@@ -2,7 +2,7 @@
 
 # MIT License
 
-# Copyright (c) 2023 Hoel Kervadec
+# Copyright (c) 2024 Hoel Kervadec
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
